1edb4585b58a8cc88ddf28f186fb14e2c7d2b957
SOS <SOS@DESKTOP-T28PJOJ>
2021-03-04 16:43:45 +0900

    21.3.4 16:43 finish

--- 06_Class&Method/src/method/Ex07.java
+++ 06_Class&Method/src/method/Ex07.java
@@ -25,9 +25,15 @@ public class Ex07 {
 		return "hi!!!!!!";
 	}
 
+	// 반환되는 값이 문자열 변수가 변환이 되는 경우
 	public static String method5(String a, String b) {
 		String result = a + b;
 		return result;
+	}
+
+	// 반환되는 값이 문자열 결합(+) 수식이 반환이 되는 경우
+	public static String method6(String a, String b) {
+		return a + b;
 	}
 
 	public static void main(String[] args) {
@@ -44,6 +50,8 @@ public class Ex07 {
 		System.out.println("method4() 메서드 호출 후 반환된 값 >>> " + method4());
 
 		System.out.println("method5() 메서드 호출 후 반환된 값 >>> " + method5("대한", "민국"));
+
+		System.out.println("method6() 메서드 호출 후 반환된 값 >>> " + method6("유", "관순"));
 
 		System.out.println("프로그램 종료");
 	}
