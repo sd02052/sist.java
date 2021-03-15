f8fd157aa8d7ff86186fd69f2e79c49db4781bb1
sist <sist@DESKTOP-216D9JE>
2021-03-15 17:39:27 +0900

    21.3.15 finish

--- 08_Exception/src/Exam01/ProductSearch.java
+++ 08_Exception/src/Exam01/ProductSearch.java
@@ -5,14 +5,15 @@ import javax.swing.JOptionPane;
 public class ProductSearch {
 	public static void main(String[] args) {
 
+		String target = JOptionPane.showInputDialog("검색할 상품명을 입력하세요.");
 		ProductSearchData psd = new ProductSearchData();
-		String target = "";
+		String prodInfo = psd.search(target);
 
 		try {
-			target = JOptionPane.showInputDialog("검색할 상품명을 입력하세요.");
+			prodInfo.length(); // 예외가 발생할 가능성이 있는 코드
 			JOptionPane.showMessageDialog(null, psd.search(target));
-
 		} catch (Exception e) {
+			System.out.println("해당 상품은 없는 상품입니다.");
 			JOptionPane.showMessageDialog(null, "해당 상품은 없는 상품입니다.");
 		}
 
